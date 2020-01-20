a348f8e4e15f8f6a9cb4f0759eaa05d58d77af24
Steve Coulter <stevecoulter99@gmail.com>
2020-01-19 19:45:16 -0600

    save and del messages

--- src/main/java/TelegramUI/Events/FormResponseEvent.java
+++ src/main/java/TelegramUI/Events/FormResponseEvent.java
@@ -8,6 +8,8 @@ import cn.nukkit.event.Listener;
 import cn.nukkit.event.player.PlayerFormRespondedEvent;
 import cn.nukkit.form.response.FormResponseCustom;
 import cn.nukkit.form.response.FormResponseModal;
+import cn.nukkit.form.response.FormResponseSimple;
+import cn.nukkit.form.window.FormWindowModal;
 import cn.nukkit.utils.TextFormat;
 
 import java.util.ArrayList;
@@ -33,6 +35,43 @@ public class FormResponseEvent implements Listener {
                         event.getPlayer().showFormWindow(plugin.formWindowMap.get("message_send"), 2);
                     case 1:
                         event.getPlayer().showFormWindow(plugin.formWindowMap.get("telegram_send"), 3);
+                        break;
+                }
+            } else if (event.getFormID() == 5) {
+                int button = response.getClickedButtonId();
+                switch (button) {
+                    case 0:
+                        FormWindowModal formWindow = (FormWindowModal) event.getWindow();
+                        Map<String, Object> objectMap = DatabaseHandler.query(event.getPlayer().getUniqueId().toString(), "uuid");
+                        List<Object> mailList = (List<Object>) objectMap.get("mail");
+                        for (int i = 0; i < mailList.size(); i++) {
+                            List<Object> mailData = (List<Object>) mailList.get(i);
+                            String headline = TextFormat.UNDERLINE+mailData.get(2).toString();
+                            String body = mailData.get(3).toString();
+                            String content = headline+"\n\n"+body;
+                            if (formWindow.getContent().equals(content)) {
+                                mailData.set(0, false);
+                                mailList.set(i, mailData);
+                            }
+                        }
+                        event.getPlayer().sendMessage(TextFormat.GREEN+"Saved" + TextFormat.WHITE + "1" + TextFormat.GREEN + "message...");
+                        DatabaseHandler.update(event.getPlayer().getUniqueId().toString(), "mail", mailList);
+                        break;
+                    case 1:
+                        FormWindowModal formWindow1 = (FormWindowModal) event.getWindow();
+                        Map<String, Object> objectMap1 = DatabaseHandler.query(event.getPlayer().getUniqueId().toString(), "uuid");
+                        List<Object> mailList1 = (List<Object>) objectMap1.get("mail");
+                        for (int i = 0; i < mailList1.size(); i++) {
+                            List<Object> mailData = (List<Object>) mailList1.get(i);
+                            String headline = TextFormat.UNDERLINE+mailData.get(2).toString();
+                            String body = mailData.get(3).toString();
+                            String content = headline+"\n\n"+body;
+                            if (formWindow1.getContent().equals(content)) {
+                                mailList1.remove(i);
+                            }
+                        }
+                        event.getPlayer().sendMessage(TextFormat.RED+"Discarded" + TextFormat.WHITE + "1" + TextFormat.RED + "message...");
+                        DatabaseHandler.update(event.getPlayer().getUniqueId().toString(), "mail", mailList1);
                         break;
                 }
             }
@@ -107,12 +146,33 @@ public class FormResponseEvent implements Listener {
                     mailData.add(2, title);
                     mailData.add(3, message);
                     telegram.add(mailData);
-                    System.out.println(telegram);
                     DatabaseHandler.update(uuid, "mail", telegram);
                     player.sendMessage("Sent telegram to => " + name);
                 }
             }
-            //return;
+            return;
         }
+
+        if (event.getResponse() instanceof FormResponseSimple) {
+            if (event.getFormID() == 4) {
+                FormResponseSimple response = (FormResponseSimple) event.getResponse();
+                Map<String, Object> objectMap = DatabaseHandler.query(event.getPlayer().getUniqueId().toString(), "uuid");
+                List<List<Object>> mail = (List<List<Object>>) objectMap.get("mail");
+                int button = response.getClickedButtonId();
+                String from = mail.get(button).get(1).toString();
+                String title = "From... "+TextFormat.ITALIC+from;
+                String headline = TextFormat.UNDERLINE+mail.get(button).get(2).toString();
+                String body = mail.get(button).get(3).toString();
+                String content = headline+"\n\n"+body;
+                viewTelegram(event.getPlayer(), title, content);
+            }
+        }
+    }
+
+    private void viewTelegram(Player player, String title, String content) {
+        String bt1 = TextFormat.DARK_GREEN+"Save";
+        String bt2 = TextFormat.DARK_RED+"Discard";
+        FormWindowModal formWindowModal = new FormWindowModal(title, content, bt1, bt2);
+        player.showFormWindow(formWindowModal, 5);
     }
 }
